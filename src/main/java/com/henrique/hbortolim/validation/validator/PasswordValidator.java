b93970d48b3efdb55f85ef8f4d628d3f998fdb47
Henrique Bortolim <henrique.bortolim@ab-inbev.com>
2025-07-06 21:49:49 -0300

    Replace negative anyMatch by stream noneMatch

--- src/main/java/com/henrique/hbortolim/validation/validator/PasswordValidator.java
+++ src/main/java/com/henrique/hbortolim/validation/validator/PasswordValidator.java
@@ -27,28 +27,28 @@ public class PasswordValidator implements ConstraintValidator<ValidPassword, Str
             return false;
         }
         
-        if (requireUpperCase && !password.chars().anyMatch(Character::isUpperCase)) {
+        if (requireUpperCase && password.chars().noneMatch(Character::isUpperCase)) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("Password must contain at least one uppercase letter")
                 .addConstraintViolation();
             return false;
         }
         
-        if (requireLowerCase && !password.chars().anyMatch(Character::isLowerCase)) {
+        if (requireLowerCase && password.chars().noneMatch(Character::isLowerCase)) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("Password must contain at least one lowercase letter")
                 .addConstraintViolation();
             return false;
         }
         
-        if (requireDigit && !password.chars().anyMatch(Character::isDigit)) {
+        if (requireDigit && password.chars().noneMatch(Character::isDigit)) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("Password must contain at least one digit")
                 .addConstraintViolation();
             return false;
         }
         
-        if (requireSpecialChar && !password.chars().anyMatch(ch -> "!@#$%^&*()_+-=[]{}|;:,.<>?".indexOf(ch) >= 0)) {
+        if (requireSpecialChar && password.chars().noneMatch(ch -> "!@#$%^&*()_+-=[]{}|;:,.<>?".indexOf(ch) >= 0)) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("Password must contain at least one special character")
                 .addConstraintViolation();
